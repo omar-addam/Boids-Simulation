e45846f76057ed7c0469813c924ec6da2451fc80
omaddam <omaddam@gmail.com>
2021-01-02 16:05:20 -0700

    Added a method that creates birds.

--- Assets/Boids/Scripts/Flock.cs
+++ Assets/Boids/Scripts/Flock.cs
@@ -51,6 +51,39 @@ namespace Broids
                 GameObject.Destroy(bird.transform);
         }
 
+        /// <summary>
+        /// Adds a new bird to the flock.
+        /// </summary>
+        private void CreateBird()
+        {
+            // Initialize list
+            if (_Birds == null)
+                _Birds = new List<Bird>();
+
+            // Create new bird
+            GameObject bird = GameObject.Instantiate(BirdTemplate, BirdsParent.transform);
+
+            // Extract its script
+            Bird birdScript = bird.GetComponent<Bird>();
+            _Birds.Add(birdScript);
+
+            // Set random location
+            bird.transform.localPosition = new Vector3
+            (
+                Random.Range(-2f, 2f),
+                Random.Range(-2f, 2f),
+                Random.Range(-2f, 2f)
+            );
+
+            // Set random rotation
+            bird.transform.localEulerAngles = new Vector3
+            (
+                Random.Range(0, 360),
+                Random.Range(0, 360),
+                Random.Range(0, 360)
+            );
+        }
+
         #endregion
 
     }
