b0fec33e64f41245a0eb616affb36d5408252ccb
omaddam <omaddam@gmail.com>
2021-01-02 15:22:51 -0700

    Referenced the template to be used when creating birds.

--- Assets/Boids/Scripts/Flock.cs
+++ Assets/Boids/Scripts/Flock.cs
@@ -7,7 +7,16 @@ namespace Broids
     public class Flock : MonoBehaviour
     {
 
+        #region Fields/Properties
 
+        /// <summary>
+        /// The prefab template used to generate birds in this flock.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("The prefab template used to generate birds in this flock")]
+        private GameObject BirdTemplate;
+
+        #endregion
 
     }
 }
