074f7892dd3eaef80fbdcb22b2d718d5c891cd66
omaddam <omaddam@gmail.com>
2021-01-02 02:39:36 -0700

    Referenced the rigid body in the script.l

--- Assets/Boids/Scripts/Bird.cs
+++ Assets/Boids/Scripts/Bird.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
 
+    #region Initialization
 
+    /// <summary>
+    /// Executes once on start.
+    /// </summary>
+    private void Awake()
+    {
+        // Extract rigid body
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    #endregion
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// References the rigidbody attached to this object.
+    /// </summary>
+    private Rigidbody Rigidbody;
+
+    #endregion
 
 }
