ed98012f64bd38e8338db7f71f224ff90a056dec
omaddam <omaddam@gmail.com>
2021-01-02 15:37:29 -0700

    Added a parent to hold all the generated birds.

--- Assets/Boids/Scripts/Flock.cs
+++ Assets/Boids/Scripts/Flock.cs
@@ -15,8 +15,15 @@ namespace Broids
         /// The prefab template used to generate birds in this flock.
         /// </summary>
         [SerializeField]
-        [Tooltip("The prefab template used to generate birds in this flock")]
+        [Tooltip("The prefab template used to generate birds in this flock.")]
         private GameObject BirdTemplate;
+
+        /// <summary>
+        /// The parent holding all the generated birds.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("The parent holding all the generated birds.")]
+        private GameObject BirdsParent;
 
         /// <summary>
         /// List of all the birds in this flock.
