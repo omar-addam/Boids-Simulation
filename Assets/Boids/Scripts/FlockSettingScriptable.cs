5b845c9fa924b05556e37efaab4dffa908e4b4b7
omaddam <omaddam@gmail.com>
2021-01-03 01:16:34 -0700

    Created a scriptable object that contains the flock's settings.

---
+++ Assets/Boids/Scripts/FlockSettingScriptable.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Broids
+{
+    [CreateAssetMenu(fileName = "FlockSettings", menuName = "ScriptableObjects/FlockSettingsScriptableObject", order = 1)]
+    public class FlockSettingScriptable : ScriptableObject
+    {
+
+        [Header("General")]
+
+        /// <summary>
+        /// The minimum speed a bird can fly.
+        /// </summary>
+        [Tooltip("The minimum speed a bird can fly.")]
+        public float MinSpeed = 1;
+
+        /// <summary>
+        /// The maximum speed a bird can fly.
+        /// </summary>
+        [Tooltip("The maximum speed a bird can fly.")]
+        public float MaxSpeed = 2.5f;
+
+        /// <summary>
+        /// The maximum steering force that can be applied at any frame rate.
+        /// </summary>
+        [Tooltip("The maximum steering force that can be applied at any frame rate.")]
+        public float MaxSteerForce = 1.5f;
+
+
+
+        [Header("Seperation Force")]
+
+        /// <summary>
+        /// The weight applied to the seperation steering force.
+        /// </summary>
+        [Tooltip("The weight applied to the seperation steering force.")]
+        public float SeperationForceWeight = 1;
+
+        /// <summary>
+        /// The distance used to find nearby birds that we need to keep distance from.
+        /// </summary>
+        [Tooltip("The distance used to find nearby birds that we need to keep distance from.")]
+        public float SeperationRadiusThreshold = 1;
+
+
+
+        [Header("Alignment Force")]
+
+        /// <summary>
+        /// The weight applied to the alignment steering force.
+        /// </summary>
+        [Tooltip("The weight applied to the alignment steering force.")]
+        public float AlignmentForceWeight = 1;
+
+        /// <summary>
+        /// The distance used to find nearby birds that we need to stay aligned with.
+        /// </summary>
+        [Tooltip("The distance used to find nearby birds that we need to stay aligned with.")]
+        public float AlignmentRadiusThreshold = 2;
+
+
+
+        [Header("Collision Avoidance Force")]
+
+        /// <summary>
+        /// The weight applied to the collision avoidance steering force.
+        /// </summary>
+        [Tooltip("The weight applied to the collision avoidance steering force.")]
+        public float CollisionAvoidanceForceWeight = 5;
+
+        /// <summary>
+        /// The distance used to find nearby obstacles that we need to avoid.
+        /// </summary>
+        [Tooltip("The distance used to find nearby obstacles that we need to avoid.")]
+        public float CollisionAvoidanceRadiusThreshold = 1;
+
+        /// <summary>
+        /// The radius that fits the bird's prefab.
+        /// </summary>
+        [Tooltip("The radius that fits the bird's prefab.")]
+        public float CollisionAvoidanceBoundsRadius = 0.5f;
+
+    }
+}
