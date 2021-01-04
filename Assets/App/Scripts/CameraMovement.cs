e79cd933cb92805f6ca0b92d7bc00cd7ee398ccd
omaddam <omaddam@gmail.com>
2021-01-03 23:42:04 -0700

    Added zoom fields.

--- Assets/App/Scripts/CameraMovement.cs
+++ Assets/App/Scripts/CameraMovement.cs
@@ -5,6 +5,29 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
+    #region Fields/Properties
 
+    /// <summary>
+    /// The minimum distance a camera can keep with the target when zooming in.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The minimum distance a camera can keep with the target when zooming in.")]
+    private float MinZoom = 0.5f;
+
+    /// <summary>
+    /// The maximum distance a camera can keep with the target when zooming out.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The maximum distance a camera can keep with the target when zooming out.")]
+    private float MaxZoom = 1f;
+
+    /// <summary>
+    /// The current distance the camera is keeping with the target.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The current distance the camera is keeping with the target.")]
+    private float Zoom = 1f;
+
+    #endregion
 
 }
