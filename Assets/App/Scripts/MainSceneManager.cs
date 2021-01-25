13e491eeb2db38b6981512602720b66d012cd7ba
omaddam <omaddam@gmail.com>
2021-01-24 23:38:45 -0700

    Displayed the app version.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -1,10 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneManager : MonoBehaviour
 {
 
+    #region Initialization
 
+    /// <summary>
+    /// Executes once on start.
+    /// </summary>
+    private void Start()
+    {
+        // Display the app version
+        DisplayVersion();
+    }
+
+    #endregion
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// Text UI element displaying the app version.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("Text UI element displaying the app version.")]
+    private Text Version;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Displays current project's version.
+    /// </summary>
+    private void DisplayVersion()
+    {
+        Version.text = string.Format("Version: {0}", Application.version);
+    }
+
+    #endregion
 
 }
